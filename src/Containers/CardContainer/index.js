023ddc26312a6a73f5e0152168ba8e01e23e17a4
Alexander Ela <ela.alexander@gmail.com>
2019-06-21 17:50:17 -0600

    Render basic CardContainer component to page

---
+++ src/Containers/CardContainer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardContainer.css';
+
+const CardContainer = (props) => {
+	return (
+		<div className='CardContainer'>
+			<h3>CardContainer</h3>
+		</div>
+	)
+}
+
+export default CardContainer;
