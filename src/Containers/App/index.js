60f3c615edeba3575dc0638480b5654ff0dcc0b1
Alexander Ela <ela.alexander@gmail.com>
2019-06-21 18:03:56 -0600

    Render basic footer to page

--- src/Containers/App/index.js
+++ src/Containers/App/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import StateList from '../../Components/StateList';
+import CardContainer from '../CardContainer';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
+      <div className="App-header-container">
         <h1>Track My States</h1>
       </div>
       <StateList />
+      <CardContainer />
+      <div className="App-footer-container">
+        <h3>App Footer</h3>
+      </div>
     </div>
   );
 }
